048a4a9ad122bf1c810b70c50aaef77dd6dc4035
HEDGEWIZARDS Drew <hedgewizardsdev@gmail.com>
2026-07-23 15:41:07 -0500

    enable MESSAGE_CONTENT intent in code

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ load_dotenv()
 
 token = os.getenv("DARCY_TOKEN")
 debug_scope = os.getenv("DARCY_DEBUG_SCOPE")
+intents = interactions.Intents.DEFAULT | interactions.Intents.MESSAGE_CONTENT
 
 bot = interactions.Client(
     token=token,
-    debug_scope=int(debug_scope) if debug_scope else None
+    debug_scope=int(debug_scope) if debug_scope else None,
+    intents=intents
 )
 
 database.initialize()
